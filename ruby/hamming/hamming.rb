d55703495e5326190410ccf1223b90cd71eb44f3
Alix Hamilton <alix.hamilton@gmail.com>
2016-08-21 22:18:38 -0700

    completed hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,22 @@
+module BookKeeping
+  VERSION = 3
+end
+
 class Hamming
 
   def self.compute(a,b)
-    0 if a == b
+    distance = 0
+
+    if a.length != b.length
+      raise(ArgumentError)
+    else
+      a.length.times do |i|
+  	     distance += 1 if a[i] != b[i]
+      end
+    end
+
+    return distance
+
   end
 
 end
