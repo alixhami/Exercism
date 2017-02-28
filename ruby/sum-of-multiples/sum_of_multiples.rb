f81b32e366047726930fec658d7edc549ada493c
Alix Hamilton <alix.hamilton@gmail.com>
2017-02-27 21:59:05 -0800

    refactored to remove use of #each

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -5,14 +5,11 @@ class SumOfMultiples
   end
 
   def to(limit)
-    total = 0
-    return total if limit < 2
-    (2..limit-1).each do |i|
-      is_multiple = false
-      @factors.each { |factor| is_multiple = true if i % factor == 0 }
-      total += i if is_multiple
-    end
-    total
+    (2...limit).select { |n| multiple?(n) }.inject(0, :+)
+  end
+
+  def multiple?(n)
+    @factors.any? { |factor| n % factor == 0 }
   end
 
 end
