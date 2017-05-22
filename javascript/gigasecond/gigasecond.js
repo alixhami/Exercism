60409a8d99a23366eddd29004df577d8ddb375ee
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-22 07:41:07 -0700

    initial solution to gigasecond

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,8 @@
+var Gigasecond = function (date) {
+  this.startDate = date;
+  this.date = function () {
+    return new Date(this.startDate.getTime() + 1000000000000);
+  };
+};
+
+module.exports = Gigasecond;
