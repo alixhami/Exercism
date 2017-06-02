ca26ed2bfa86b07284e81710b1994d8b558d147a
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-02 15:21:21 -0700

    refactors to base game on frames allowed instead of frames remaining, adds comments

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -5,47 +5,52 @@ class Game
     @frames = [[]]
     @current = 0
     @throws_remaining_in_frame = 2
-    @frames_remaining = 10
+    @frames_allowed = 10
   end
 
   def roll(pins)
     new_frame_total = @frames[@current].inject(0, :+) + pins
     raise BowlingError unless valid_roll?(pins, new_frame_total)
 
+    # adds pins to current frame and decrements the throws remaining
     @frames[@current] << pins
     @throws_remaining_in_frame -= 1
 
     # adds a bonus frame if the 10th frame is a strike or spare
     # or adds a second bonus frame if the 10th & 11th frames are strikes
-    if (new_frame_total == 10 && @current == 9) || (@frames[9..10] == [[10],[10]] && @current == 10)
-      @frames_remaining += 1
-
-    # ends game after last throw of bonus round
-    elsif (@current == 10 && !strike?(@frames[9])) || @current == 11
-      @frames_remaining = 0
+    if (new_frame_total == 10 && @current == 9) || (@frames[9..10] == [[10], [10]] && @current == 10)
+      @frames_allowed += 1
     end
 
-    next_frame if (@throws_remaining_in_frame == 0 || pins == 10) && @frames_remaining != 0
+    # start a new frame if the frame is complete and the game isn't over
+    if !complete_game? && frame_complete?(@frames[@current], @throws_remaining_in_frame)
+      next_frame
+    end
   end
 
   def score
-    raise BowlingError unless complete?
+    # only allow a complete game to be scored
+    raise BowlingError unless complete_game?
 
     score = 0
     @frames.each_with_index do |frame, index|
       # won't double count the bonus frame(s)
       break if index > 9
 
+      # add the sum of the frame's pins to the score
       pin_total = frame.inject(:+)
       score += pin_total
 
-      # bonus points
+      # add strike/spare bonus points
       if spare?(frame)
+        # add the next roll after a spare
         score += @frames[index + 1][0]
       elsif strike?(frame)
+        # add the next two rolls (frame) for a strike
         score += @frames[index + 1].inject(:+)
 
-        # counts the second roll after if the next roll is a strike
+        # adds an additional throw if the next frame only has one throw
+        # (two strikes in a row)
         score += @frames[index + 2][0] if strike?(@frames[index + 1])
       end
     end
@@ -62,8 +67,12 @@ class Game
     frame.length == 2 && frame.inject(:+) == 10
   end
 
-  def complete?
-    @frames_remaining == 0
+  def frame_complete?(frame, throws_remaining)
+    frame == [10] || throws_remaining == 0
+  end
+
+  def complete_game?
+    @frames.length == @frames_allowed && frame_complete?(@frames.last, @throws_remaining_in_frame)
   end
 
   def next_frame
@@ -76,11 +85,10 @@ class Game
 
     @current += 1
     @frames[@current] = []
-    @frames_remaining -= 1
   end
 
   def valid_roll?(pins, new_frame_total)
-    !complete? && (0..10).include?(pins) && new_frame_total <= 10
+    !complete_game? && (0..10).include?(pins) && new_frame_total <= 10
   end
 end
 
