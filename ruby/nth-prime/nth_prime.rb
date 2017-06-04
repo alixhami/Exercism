21ee4caacd376c0ae0f504f7abf6a546c44400eb
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-03 23:07:32 -0700

    Refactored prime? method to use memoized primes as factors to check; test runtime down to 3.8s

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,22 +1,22 @@
 class Prime
-  @@primes = [2]
+  @@primes = [2, 3]
 
   def self.nth(n)
-    raise ArgumentError if n == 0
+    raise ArgumentError if n < 1
 
-    next_number = @@primes.last + 1
+    next_number = @@primes.last + 2
     while @@primes.length < n
       @@primes << next_number if prime?(next_number)
-      next_number += 1
+      next_number += 2
     end
-    
+
     @@primes[n-1]
   end
 
   private
   def self.prime?(number)
-    (2..number/2).each do |i|
-      return false if number % i == 0
+    @@primes.all? do |prime|
+      number % prime != 0
     end
   end
 end
