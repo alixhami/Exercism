348f8a653685aefa53a88337cdde1d9fc2c1ebfe
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-03 22:09:01 -0700

    while loop version of run-length-encoding

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,37 +1,56 @@
 class RunLengthEncoding
 
   def self.encode(input)
-    count = 0
-    encoded_input = ''
-    (input.length).times do |i|
-      if (i == 0 && input[i] != input[i+1]) || (input[i] != input[i+1] && input[i] != input[i-1])
-        encoded_input += input[i]
-      elsif i == 0 || input[i] == input[i+1] && input[i] != input[i-1]
-        count = 1
-      elsif input[i] == input[i+1] && input[i] == input[i-1]
+    letter = input[0]
+    i = 0
+    encoded = ""
+
+    while i < input.length
+      count = 0
+
+      # while the current character is repeating
+      while input[i] == letter
         count += 1
-      elsif input[i] == input[i-1] && input[i] != input[i+1]
-        count += 1
-        encoded_input += count.to_s + input[i]
+        i += 1
       end
+
+      # add the number (unless it is 1), followed by the character
+      encoded += "#{count.to_s unless count == 1}#{letter}"
+
+      # set the next letter as the character that didn't
+      # meet the condition to enter the while loop
+      letter = input[i]
     end
-    encoded_input
+    encoded
   end
 
   def self.decode(input)
-    decoded_input = ''
-    (input.length).times do |i|
-      if input[i].to_i == 0 && (i == 0 || input[i-1].to_i == 0)
-      	decoded_input += input[i]
-      elsif input[i].to_i != 0 && input[i+1].to_i != 0
-      	next
-      elsif input[i].to_i != 0 && input[i+1].to_i == 0 && (i == 0 || input[i-1].to_i == 0)
-      	decoded_input += input[i+1] * input[i].to_i
-      elsif input[i].to_i != 0 && i != 0 && input[i-1].to_i != 0
-      	decoded_input += input[i+1] * input[i-1..i].to_i
+    i = 0
+    decoded = ""
+
+    while i < input.length
+      num_string = ""
+
+      # while the character is a number
+      while input[i].to_i.to_s == input[i]
+        # add the number to the num_string and increment
+        num_string += input[i]
+        i += 1
       end
+
+      # the letter is the first character that doesn't meet
+      # the number conditional in the while loop
+      letter = input[i]
+
+      # repeat the character once if no numbers are found
+      num_string = 1 if num_string.empty?
+
+      # add the letter to the decoded string num_string times
+      decoded += letter * num_string.to_i
+      i += 1
     end
-    decoded_input
+
+    decoded
   end
 
 end
