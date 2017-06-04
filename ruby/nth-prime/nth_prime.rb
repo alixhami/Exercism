53141e899722aa46bd94b17d252100f76acd6b7f
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-03 22:49:45 -0700

    adds memoization; test runs are down to 16 seconds

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,20 +1,22 @@
 class Prime
+  @@primes = [2]
 
-  def self.is_prime?(number)
+  def self.nth(n)
+    raise ArgumentError if n == 0
+
+    next_number = @@primes.last + 1
+    while @@primes.length < n
+      @@primes << next_number if prime?(next_number)
+      next_number += 1
+    end
+    
+    @@primes[n-1]
+  end
+
+  private
+  def self.prime?(number)
     (2..number/2).each do |i|
       return false if number % i == 0
     end
   end
-
-  def self.nth(n)
-    raise ArgumentError if n == 0
-    primes = [2]
-    next_number = 3
-    while primes.length < n
-      primes << next_number if is_prime?(next_number)
-      next_number += 1
-    end
-    primes[n-1]
-  end
-
 end
