b4a7a2fad46c5451aca4f70a719b75bbc4931e68
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-03 23:35:14 -0700

    refactors rna-transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,22 +1,17 @@
 class Complement
+  DNA_TO_RNA_MAP = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
   def self.of_dna(dna_strand)
-    rna_strand = ''
-    (dna_strand.length).times do |i|
-      case dna_strand[i]
-      when 'G'
-        rna_strand[i] = 'C'
-      when 'C'
-        rna_strand[i] = 'G'
-      when 'T'
-        rna_strand[i] = 'A'
-      when 'A'
-        rna_strand[i] = 'U'
-      else
-        rna_strand = ''
-        break
-      end
-    end
-    rna_strand
+    rna_strand = dna_strand.chars.map { |nucleotide|
+      DNA_TO_RNA_MAP[nucleotide]
+    }.join
+
+    rna_strand.length == dna_strand.length ? rna_strand : ''
   end
 end
 
