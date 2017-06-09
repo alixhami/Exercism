7e460b4489a43056405210183ac38132790a4fff
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-08 23:12:43 -0700

    first version of matrix

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,15 @@
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(matrix_string)
+    @rows = string_to_rows(matrix_string)
+    @columns = rows.transpose
+  end
+
+  private
+  def string_to_rows(matrix_string)
+    matrix_string.split("\n").map { |row_string|
+      row_string.split(' ').map { |num| num.to_i }
+    }
+  end
+end
