ebbb42fb316be12d56260ef5a695378b6611d7e0
Alix Hamilton <alix.hamilton@gmail.com>
2017-08-06 21:33:27 -0700

    first version of hamming

---
+++ python/hamming/hamming.py
@@ -0,0 +1,8 @@
+def distance(strand1, strand2):
+    if len(strand1) != len(strand2): raise ValueError
+
+    distance = 0
+    for i in range(len(strand1)):
+        if strand1[i] != strand2[i]: distance += 1
+
+    return distance
