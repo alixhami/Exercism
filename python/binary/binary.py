a7ca19aed940748d32c0f776f3c0d74b5e14c6e8
Alix Hamilton <alix.hamilton@gmail.com>
2017-11-10 22:26:39 -0800

    refactors to reverse string and use sets

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,9 +1,5 @@
-def parse_binary(binary_string):
-    num = 0
-    for index, char in enumerate(binary_string):
-        if char == "1":
-            num += 2 ** (len(binary_string) - 1 - index)
-        elif char != "0":
-            raise ValueError("Only the characters '0' or '1' are allowed.")
+def parse_binary(string):
+    if set(string) - set('10'):
+        raise ValueError("Only the characters '0' or '1' are allowed.")
 
-    return num
+    return sum(2 ** i for i, char in enumerate(string[::-1]) if char == '1')
