68094aa50a7c5dfabbbfb7215493e6ac0b710346
Alix Hamilton <alix.hamilton@gmail.com>
2017-11-10 22:04:23 -0800

    short version within python line character limits

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,13 +1,5 @@
-from collections import Counter
+def is_match(word1, word2):
+    return word1 != word2 and sorted(word1) == sorted(word2)
 
-
-def detect_anagrams(word, candidates):
-    letter_counts = Counter(word.lower())
-    matches = []
-    for candidate in candidates:
-        if candidate.lower() == word.lower():
-            continue
-        if Counter(candidate.lower()) == letter_counts:
-            matches.append(candidate)
-
-    return matches
+def detect_anagrams(target, words):
+    return [word for word in words if is_match(target.lower(), word.lower())]
